0ad2679ba4d57347337d2844e6e1826b85e4fc11
Ruben Epong <rubenjohne@gmail.com>
2013-08-29 22:34:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sfumato::Application.config.secret_key_base = 'c2dd3dc7ccd1359bc0e013004e36b309dc5bb342cbd1cbf721fdb3d7ece6387a99fa4fdd13718d6dcd3b81d8b70a85a2bf2a16efaa136de31e3cf2b49bd218d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sfumato::Application.config.session_store :cookie_store, key: '_sfumato_session'
